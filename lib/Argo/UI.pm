50932b88acbc47ee2f03b8c61ddcfff66f157007
Masetti, Marco <marco.masetti@sky.uk>
2019-07-09 16:38:37 +0100

    input params handled

--- lib/Argo/UI.pm
+++ lib/Argo/UI.pm
@@ -18,18 +18,27 @@ get '/workflows' => sub {
     return encode_json( $argo_client->workflows_as_list() );
 };
 
+get '/workflow_tree' => sub {
+    return encode_json( $argo_client->workflows_as_tree() );
+};
+
 get '/log' => sub {
-    my $workflow_name = query_parameters->get('name');
-    my $log = $argo_client->workflow_log( $workflow_name );
+    my $id = query_parameters->get('id');
+    my $log = $argo_client->workflow_log( $id );
     my @log = split(/\n/, $log);
     shift @log;
     my $trimmed = join("\n", @log);
 
-    if ( $workflow_name =~ /^ycsb/ ) {
+    if ( $id =~ /^ycsb/ ) {
         my $y = YCSB::Metrics->new();
         $y->load_metrics( $trimmed );
     }
     return $trimmed;
+};
+
+get '/params' => sub {
+    my $name = query_parameters->get('name');
+    return encode_json( $argo_client->workflow_params( $name ) );
 };
 
 
